f5739c2ab775a023f71069d4fdb64a6533c3cd08
kirill.ivanov <kivanov@ttk.ru>
2025-02-08 16:01:35 +0300

    ТГ Бот
    Доработы сервисы получения информации по фото (достопримечательности, картины)

--- bot/routers/commands.py
+++ bot/routers/commands.py
@@ -1,26 +1,22 @@
 import json
 import os
 
+import io
+import base64
+
 import cv2
+import requests
+from PIL import Image
 from aiogram import F, Router, types
 from aiogram.client import bot
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
 
-from alg.yolo import apply_yolo_object_detection_by_url
+from config import settings
 
 router = Router(name=__name__)
 
 CURRENT_MENU = ''
-
-
-@router.message(Command("yolo"))
-async def yolo_command(message: types.Message):
-    global CURRENT_MENU
-    CURRENT_MENU = 'yolo'
-    await message.answer(
-        text="Выбран пункт меню - <b>Поиск объектов по фото (YOLO)</b>. Загрузите фото"
-    )
 
 
 @router.message(Command("sign"))
@@ -51,32 +47,56 @@ async def image_message(message: types.Message):
     isExist = os.path.exists(path)
     if not isExist:
         os.makedirs(path)
-        print("The new directory is created!")
-
-    print(f'CURRENT_MENU: {CURRENT_MENU}')
 
     file_name = f'{message.photo[-1].file_id}'
     file_dist = f'{path}/{file_name}.jpg'
-    print(f'file_dist: {file_dist}')
     await message.bot.download(message.photo[-1], destination=file_dist)
 
-    if CURRENT_MENU == 'yolo':
-        file_result, stat = apply_yolo_object_detection_by_url(file_dist)
-        cv2.imwrite(f'{path}/{file_name}_result.jpg', file_result)
+    if CURRENT_MENU == 'sign':
+        try:
+            url = settings.API_URL + "/landmark_image/"
 
-        if len(stat) > 0:
-            mess_text = "Найдено объектов:\n"
-            for key, value in stat.items():
-                mess_text += f" <b>{key}</b>: {value}\n"
-        else:
-            mess_text = "Не найдено ни одного объекта!"
+            image = Image.open(file_dist)
 
-        await message.reply_photo(photo=types.FSInputFile(f'{path}/{file_name}_result.jpg'),
-                                  caption=mess_text,
-                                  parse_mode=ParseMode.HTML)
-    elif CURRENT_MENU == 'sign':
-        await message.answer("Вызов API -> В разработке")
+            buffered = io.BytesIO()
+            image.save(buffered, format="BMP")
+            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+            with requests.Session() as client:
+                resp_rep = client.post(url, data=json.dumps({"input_image": img_str}))
+
+            if resp_rep.status_code == 200:
+                result = json.loads(json.loads(resp_rep.text)['Result'])
+                if str(result['descr']) != '':
+                    await message.answer(text=str(f"""
+                            <b>{str(result['title'])}</b>\n{str(result['url'])}\n\n{str(result['descr'])} 
+                            """), parse_mode=ParseMode.HTML)
+                else:
+                    await message.answer(text=str(f"<i>{result['title']}</i>"), parse_mode=ParseMode.HTML)
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            await message.answer(text=f"Произошла ошибка: [{e}]")
     elif CURRENT_MENU == 'paint':
-        await message.answer("Вызов API -> В разработке")
+        try:
+            url = settings.API_URL + "/museum_image/"
+
+            image = Image.open(file_dist)
+
+            buffered = io.BytesIO()
+            image.save(buffered, format="BMP")
+            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+            with requests.Session() as client:
+                resp_rep = client.post(url, data=json.dumps({"input_image": img_str}))
+
+            if resp_rep.status_code == 200:
+                result = json.loads(json.loads(resp_rep.text)['Result'])
+                if str(result['descr']) != '':
+                    await message.answer(text=str(
+                        f"""<b>{str(result['author'])}</b>. {str(result['title'])}\n\n{str(result['descr'])}"""))
+                else:
+                    await message.answer(text=str(f"<i>{result['title']}</i>"))
+        except Exception as e:
+            await message.answer(text=f"Произошла ошибка: [{e}]")
     else:
         await message.answer(text="Выберите пункт меню перед загрузкой фото")
--- bot/routers/media_handlers.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from aiogram import Router, F, types
-
-
-router = Router(name=__name__)
-
-#any_media_filter = F.photo
